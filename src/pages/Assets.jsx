0b43a84aef71ce57b4b7cdae91d4ecc970e2c784
Asibul Rafi <asibulrafi@gmail.com>
2022-09-18 02:11:22 +0600

    swiss prime

--- src/pages/Assets.jsx
+++ src/pages/Assets.jsx
@@ -1,26 +1,352 @@
 import React from "react";
-
+import assetImg1 from "../assets/assetsImg1.png";
+import assetImg2 from "../assets/assetsImg2.png";
+import commoditiesImg from "../assets/commoditiesImg.png";
+import indicesImg from "../assets/indicesImg.png";
+import algoImg from "../assets/algoImg.png";
+import one from "../assets/algoIcon1.png";
+import two from "../assets/algoIcon2.png";
+import three from "../assets/algoIcon3.png";
 export default function Assets() {
   return (
     <>
-      <section className="about">
-        <div className="about__wrapper">
-          <h2 className="about__heading">About Us</h2>
-          <div className="about__content">
-            <div className="about__content-info"></div>
-            <div className="about__content-img">
-              <img src="" alt="" />
+      <section className="assets">
+        <div className="assets__wrapper">
+          <h2 className="assets__heading">Assets</h2>
+          <div className="assets__content">
+            <div className="assets__content-info">
+              <h2>Stock CFD Trading</h2>
+              <p>
+                Stocks CFD trading helps you to enter the many major stock
+                markets from different countries avoiding large investments and
+                gains bigger advantages that trading just with assets by
+                themselves. Waysfx arranges also many other benefits for
+                traders, delivering them the premium terms for trade with no
+                charges for almost all trading services. Low spread and the
+                leverage will help you even maximize the profit.{" "}
+              </p>
+              <p>
+                The absence of short sales bounds, stock lending, and borrowing
+                or other confines picture Stock CFD market very attractive for
+                trading. You will be excited about you trading process that
+                should rely on indecisive stock prices, fluctuating macro- and
+                microeconomics factors and many others technological, political
+                and natural processes
+              </p>
+              <p>
+                Companies can increase their money income by launch selling
+                their shares/stocks for growth, new products, development and so
+                on to the public. Investors can take a revenue of the huge
+                companies and gain a stock profit. Such giant companies, like
+                Google, Apple, Intel and so on are selling their shares online.
+                The forehand goal of No Name 1 is to make sure that all
+                customers have the best support for successful trading results.
+                Following that goal, we have developed for you this platform
+                which is supplemented with all possible trading features, tools
+                and provide you with all broadcasts and updated events by a team
+                of our dedicated professionals.
+              </p>
+            </div>
+            <div className="assets__content-img">
+              <img src={assetImg1} alt="assetImg" />
+            </div>
+          </div>
+          <div className="assets__content">
+            <div className="assets__content-info">
+              <h2>What is Forex</h2>
+              <p>
+                The definition of Forex is the trading of different currency
+                pairs. The trading occurs between one currency against another.
+                One currency is compared with the other. For example, the rate
+                of the USD/GBP currency pair is 0.66, then it means that 1 USA
+                dollar costs 0.66 British Pounds. The goal of Forex is to see if
+                the American dollar will enhance or not its price equated to the
+                British Pound. If it does, then the trader should buy the USDGBP
+                pair on this term.
+              </p>
+              <p>
+                In the case when the price goes down, for instance from 0.66 to
+                0.64, then the USA dollar has a descended position. If the price
+                moves in the opposite way, for example from 0.66 to 0.69, then
+                the USA dollar is increasing its price against the British
+                Pound. You will meet often the terms “short” and “lot” where
+                “long”, in that instance, correlate with the Dollar and “short”
+                refers to the British Pound. Because the GBP stated its price in
+                the USD. The other important expression that you must take into
+                consideration is “the major”, which signify the ruling global
+                currency pairs, such as the AUDUSD, the USDJPY, the EURUSD, the
+                USDCAD, the USDCHF and the GBPUSD. And every one of them has the
+                Dollar inside. We have the phrase “cross pairs” which refers to
+                pairs that do not have the Dollar in them.{" "}
+              </p>
+              <p>
+                Forex does not function as a real physical currency transaction.
+                All transitions and swaps are going on the Internet, which means
+                that this market works only as speculation with currencies.
+                Approximate statistic says that more over 4 trillion dollars are
+                being swapped every day all around the globe, but all the assets
+                are engaged in many types – future, forward, currency exchange
+                and so on.
+              </p>
+              <p>
+                The Forex trading works without breaks, during the whole week
+                and 24 hours a day, but it has holidays on weekends. Look for
+                additional Forex information at the Waysfx learning section.
+              </p>
+            </div>
+            <div className="assets__content-img">
+              <img src={assetImg2} alt="assetImg" />
             </div>
           </div>
         </div>
       </section>
-      <section className="about">
-        <div className="about__wrapper">
-          <h2 className="about__heading">About Us</h2>
-          <div className="about__content">
-            <div className="about__content-info"></div>
-            <div className="about__content-img">
-              <img src="" alt="" />
+      <section className="commodities">
+        <div className="commodities__wrapper">
+          <div className="commodities__content">
+            <div className="commodities__content-info">
+              <h2>Commodities</h2>
+              <p>
+                The biggest No name 1 priority is to make sure that a platform
+                supplies to its precious customers the broadest feasible
+                selection of assets, and the commodities asset is not an
+                exclusion. Guiding you every single step of a trading process
+                and ensuring that you have the best of possible trading
+                conditions during all the time are other important mis sions for
+                us.
+              </p>
+              <h2>No name 1 allows you:</h2>
+              <ul>
+                <li>Invest in precious metal, such as gold or silver</li>
+                <li>Make contractual agreements</li>
+                <li>Make contracts on natural resources</li>
+                <li>Invest in the energy complex</li>
+              </ul>
+            </div>
+            <div className="commodities__content-img">
+              <img src={commoditiesImg} alt="commoditiesImg" />
+            </div>
+          </div>
+          <div className="commodities__info">
+            <h2 className="commodities__info-heading">
+              The platform enables you to trade with
+              <br /> such list of assets:
+            </h2>
+            <div className="commodities__info-content">
+              <div className="commodities__info-content-box">
+                <h2>GOLD</h2>
+                <p>
+                  Recently, there has been seeing such a big jump in the volume
+                  of gold and trades with it. Gold is seen as a quite gainful
+                  commodity by a lot of investors. Waysfx provides you with the
+                  ability to invest in gold globally, from any point on the
+                  Earth, and you don’t have to make a large physical purchase of
+                  big gold quantities. Gold has always been a chief economic
+                  influence, and it will develop in the future as well. You
+                  should bear this in a mind just because gold is strongly
+                  tangled with many currencies. Gold could have a strong and
+                  considerable impact on its value.
+                </p>
+                <p>
+                  You have the choice to trade Gold with the US Dollar, which is
+                  what most people decide to do, but if you`d like you might as
+                  well to do the same with the Euro or another existing
+                  currency. To make a good trading income, trading gold has
+                  always had a lot of potentials.
+                </p>
+              </div>
+              <div className="commodities__info-content-box">
+                <h2>SILVER</h2>
+                <p>
+                  Worthily said that silver has a twin economic nature. First,
+                  it is a precious metal, but it also has the industrial
+                  usability, which is one of the most notifiable factors to
+                  invest in it. When the price of gold is hesitating, a silver,
+                  as a precious metal, can be experienced a positive trend. You
+                  can see the same with currency pairs that fluctuate on the
+                  market and could make a nice impact on the silver value.
+                  Silver is highly demanded usually, specifically in the solar
+                  arenas.
+                </p>
+              </div>
+              <div className="commodities__info-content-box">
+                <h2>OIL</h2>
+                <p>
+                  The use of crude oil usually intends on diversification of
+                  one’s profile that help to detract the risk of a loss. What
+                  works best for this is a quite unique nature of that asset,
+                  which makes it stand out from equity and currency trading.
+                  Inventory factors, OPEC reports or climate changes are not the
+                  only factors that you need to think of trading oil. But, it is
+                  the second most traded commodity after gold and it has an
+                  impressive number of turnovers every day.
+                </p>
+              </div>
+              <div className="commodities__info-content-box">
+                <h2>NATURAL GAS</h2>
+                <p>
+                  Natural gas is quite a rare resource and we are often unsure
+                  about its overall quantity. This asset is very hard to predict
+                  due to countless political circumstances, unstable climate and
+                  many innovations regarding its excavation and discovery. It is
+                  usually recommended to make a large research and develop the
+                  strategy before investing in gas. In that case, Waysfx has
+                  every day non-stop service support for its users. A team of
+                  our dedicated professionals handles any inquiries or issues
+                  about trading this and other commodities.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="indices">
+        <div className="indices__wrapper">
+          <div className="indices__content">
+            <div className="indices__content-info">
+              <h2>Indices</h2>
+              <p>
+                No Name1 offers to invest in indices from any place in the globe
+                in super flexible and easy way. An index is a general
+                performance indicator for the essential cluster of stocks which
+                can be sorted by location, exchange point or many other things.
+                Major Stock Index section is responsible for this and it also
+                supports traders to see how a certain stock group is forming.
+                Among many of others, you can find the most traded indices such
+                as the NASDAQ, S&P 500, FTSE and the Dow Jones.
+              </p>
+            </div>
+            <div className="indices__content-img">
+              <img src={indicesImg} alt="indicesImg" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="algo">
+        <div className="algo__wrapper">
+          <div className="algo__content">
+            <div className="algo__content-info">
+              <h2>Algo Trading</h2>
+              <p>
+                Algo Trading becomes very popular nowadays due to its active
+                development. It is a program for investors to become more
+                successful, have favorable outcomes and constantly evolve. Algo
+                Trading is based on the artificial intelligence and
+                scenario-based approach that help investors have profitable
+                trading. The time period, quantity and price are, of course,
+                taken into account while creating trading strategies.
+              </p>
+              <p>
+                Fast multiple investing – the quantity of trading operations the
+                Algo Trading is able to perform is way higher and differs from
+                any regular trader. The software may proceed with repeated tasks
+                through a certain time period and this may take place in various
+                markets. Of course, all taken actions are intended to improve
+                investor’s trading progress 24/7 – you won’t have to plan your
+                trading and follow any platform’s working curfews, the software
+                offers its services non-stop to draw more traders and help them
+                get more profit any time, suitable for them.
+              </p>
+              <p>
+                Mechanized work – as we are using artificial intelligence and
+                specific algorithms, all our investors should not worry about
+                human factor (stress, personal problems, unfocused work etc)
+                that may cause any losses.
+              </p>
+              <p>
+                Seeing all cards – investors can take their time and build a
+                strategy, see through all pros and cons of the trading and
+                future actions because there will be no need to control the
+                system that will be automatically doing its job and handling all
+                “unnecessary” things.
+              </p>
+            </div>
+            <div className="algo__content-img">
+              <img src={algoImg} alt="algoImg" />
+            </div>
+          </div>
+          <div className="algo__info">
+            <div className="algo__info-box">
+              <h2>Various robot-functions</h2>
+              <p>
+                Depending on the chosen risk profile, all robots have the same
+                mechanism: they have various setting options to choose.The
+                trading principle here is as usual: the more you set on the
+                table (and accordingly the risks get higher), the more you may
+                get as a reward.
+              </p>
+              <ul>
+                <li>
+                  Auto-search – the system will constantly update the market and
+                  search for the best trading opportunities and options
+                  according to the inquiry and will send notifications right
+                  away
+                </li>
+                <li>
+                  Make trades – the robot will assist the investor to create a
+                  number of trades right away. All actions will be done in
+                  accordance with the specific instructions, set by the trader,
+                  and will also correspond to the specific risk margins
+                </li>
+                <li>
+                  The strategy of the investment – the robot works according to
+                  the set algorithm, which helps to find best deals, best
+                  opportunities on the market. It also is done in accordance
+                  with the special user’s preferences
+                </li>
+              </ul>
+            </div>
+            <div className="algo__info-box">
+              <h2>
+                The assistance of the <br />
+                software will be:
+              </h2>
+              <ul>
+                <li>
+                  Help detect the quality of affording gain, which will be best
+                  for investors
+                </li>
+                <li>Reduced expenses</li>
+                <li>Lucidity becomes more perceptible</li>
+                <li>
+                  Amend balance of commitment and planning of trading actionss
+                </li>
+                <li>
+                  Develop investor’s skills to analyze and take action according
+                  to the trading situation
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="algo__varient">
+            <h2 className="algo__varient-heading">
+              Three variants for trading:
+            </h2>
+            <div className="algo__varient-box">
+              <img src={one} alt="one" />
+              <p>
+                Classics of the market – you should choose this risk profile if
+                you are trading in small amounts. You will get support and
+                feedback within this profile if funds amount does not outbid 10
+                000 USD, and annual percentage rate is 3, which will surely help
+                the trader with any shortages
+              </p>
+            </div>
+            <div className="algo__varient-box">
+              <img src={two} alt="two" />
+              <p>
+                Know-how investor – this one should be chosen if you have
+                already traded for some time and you have quite a bit knowledge
+                of the trading principals. Annual percentage rate – 7, maximum
+                money amount – 50 000 USD
+              </p>
+            </div>
+            <div className="algo__varient-box">
+              <img src={three} alt="three" />
+              <p>
+                Professional businessman – on this profile terms you will have
+                the percentage rate of 25 (can you actually imagine) and the sum
+                of the account should exceed 50 000 USD
+              </p>
             </div>
           </div>
         </div>
